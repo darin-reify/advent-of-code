38b237983bff9b3285ba8e2967971dd82de08246
Darin Douglass <darin.douglass@reifyhealth.com>
2021-12-02 11:17:18 -0500

    -2 char

--- 2021/golf/day1.pl
+++ 2021/golf/day1.pl
@@ -1,1 +1,1 @@
-chomp(@a=<>);@b=map{eval join'+',@a[$_..$_+2]}0..$#a;map{$c++if@b[$_]<@b[$_+1]}0..$#b;print$c
+chomp(@a=<>);@b=map{eval join'+',@a[$_..$_+2]}0..$#a;map{$c+=@b[$_]<@b[$_+1]}0..$#b;print$c
